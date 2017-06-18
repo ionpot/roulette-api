265802ff1e5f3c8a56f05ea24167144b2dbb5a70
Emir Kotan <emirkotan@gmail.com>
2017-06-18 01:31:55 +0100

    501 test.

---
+++ test/status-codes.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var A = require("assert");
+
+var R = require("utils/readable");
+var W = require("utils/writable");
+
+var sut = require("../router.js");
+
+function makeReq(method, url, head, body) {
+	var req = R.create(body || "");
+
+	req.method = method;
+	req.url = url;
+	req.headers = head;
+
+	return req;
+}
+
+function send(req, scode, done) {
+	var res = W.create();
+	var actual;
+
+	res.writeHead = function (code) {
+		actual = code;
+	};
+
+	res.on("finish", function () {
+		A.strictEqual(actual, scode);
+
+		done();
+	});
+
+	sut(req, res);
+}
+
+suite("Status Codes");
+
+test("501 HEAD", function (done) {
+	this.timeout(500);
+
+	var req = makeReq("HEAD", "/");
+
+	send(req, 501, done);
+});
+
+test("501 PUT", function (done) {
+	this.timeout(500);
+
+	var req = makeReq("PUT", "/");
+
+	send(req, 501, done);
+});
+
+test("501 DELETE", function (done) {
+	this.timeout(500);
+
+	var req = makeReq("DELETE", "/");
+
+	send(req, 501, done);
+});
+
+test("501 OPTIONS", function (done) {
+	this.timeout(500);
+
+	var req = makeReq("OPTIONS", "/");
+
+	send(req, 501, done);
+});
